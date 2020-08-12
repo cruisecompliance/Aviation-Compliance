fea6d62378475333132fa90451ad5202b867f78f
Vadim.Kukharenko <riv4ik@gmail.com>
2020-08-12 14:30:01 +0300

    Fix: Admin/Requirement/index.blade.php (Last version of requirements)

--- resources/views/admin/requirements/index.blade.php
+++ resources/views/admin/requirements/index.blade.php
@@ -82,206 +82,207 @@
                     </div> <!-- end card -->
                 </div><!-- end col-->
             </div><!-- end row-->
-            <div class="row">
-                <div class="col-12">
-                    <div class="card">
-                        <div class="card-body">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-body">
 
-                            <div class="row">
-                                <div class="col-12">
-                                    <h4 class="page-title">Last version of requirements</h4>
+                                <div class="row">
+                                    <div class="col-12">
+                                        <h4 class="page-title">Last version of requirements</h4>
+                                    </div>
                                 </div>
-                            </div>
-
-                            <div class="row">
-                                <div class="col-12">
-                                    <table id="basic-datatable" class="table nowrap w-100">
-                                        <thead>
-                                        <tr>
-                                            <th scope="col" width="40px"></th>
-                                            <th scope="col">Sec #</th>
-                                            <th scope="col">IR/AMC/GM</th>
-                                            <th scope="col">Rule Reference</th>
-                                            <th scope="col">Rule Title</th>
-                                            <th scope="col">AMC3 ORO.MLR.100 Manual Reference</th>
-                                            <th scope="col">AMC3 ORO.MLR.100 Chapter</th>
-                                        </tr>
-                                        </thead>
-                                    </table>
+                                @if($versions->isNotEmpty())
+                                <div class="row">
+                                    <div class="col-12">
+                                        <table id="basic-datatable" class="table nowrap w-100">
+                                            <thead>
+                                            <tr>
+                                                <th scope="col" width="40px"></th>
+                                                <th scope="col">Sec #</th>
+                                                <th scope="col">IR/AMC/GM</th>
+                                                <th scope="col">Rule Reference</th>
+                                                <th scope="col">Rule Title</th>
+                                                <th scope="col">AMC3 ORO.MLR.100 Manual Reference</th>
+                                                <th scope="col">AMC3 ORO.MLR.100 Chapter</th>
+                                            </tr>
+                                            </thead>
+                                        </table>
+                                    </div>
                                 </div>
-                            </div>
+                                @endif
 
-                        </div> <!-- end card body-->
-                    </div> <!-- end card -->
-                </div><!-- end col-->
-            </div><!-- end row-->
 
-            <!-- /page content -->
+                            </div> <!-- end card body-->
+                        </div> <!-- end card -->
+                    </div><!-- end col-->
+                </div><!-- end row-->
+                <!-- /page content -->
         </div>
 
     </div>
 
-        <!-- modal -->
-        <div class="modal fade" id="ajaxModel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h4 class="modal-title" id="modelHeading"></h4>
-                    </div>
-                    <div class="modal-body">
-                        <form id="ItemForm" action="" name="ItemForm" class="form-horizontal" method="POST" enctype="multipart/form-data">
+    <!-- modal -->
+    <div class="modal fade" id="ajaxModel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title" id="modelHeading"></h4>
+                </div>
+                <div class="modal-body">
+                    <form id="ItemForm" action="" name="ItemForm" class="form-horizontal" method="POST" enctype="multipart/form-data">
 
-                            <input type="hidden" name="_method" id="_method" value="">
-                            <input type="hidden" name="Item_id" id="Item_id">
+                        <input type="hidden" name="_method" id="_method" value="">
+                        <input type="hidden" name="Item_id" id="Item_id">
 
-                            <div class="form-group">
-                                <label for="title" class="control-label">Title*</label>
-                                <input type="text" class="form-control" id="title" name="title" value="" required>
-                            </div>
+                        <div class="form-group">
+                            <label for="title" class="control-label">Title*</label>
+                            <input type="text" class="form-control" id="title" name="title" value="" required>
+                        </div>
 
-                            <div class="form-group">
-                                <label for="description" class="control-label">Description</label>
-                                <textarea class="form-control" id="description" name="description" rows="3" placeholder=""></textarea>
-                            </div>
+                        <div class="form-group">
+                            <label for="description" class="control-label">Description</label>
+                            <textarea class="form-control" id="description" name="description" rows="3" placeholder=""></textarea>
+                        </div>
 
-                            {{--                        <div class="form-group">--}}
-                            {{--                            <label>.xlsx file*</label>--}}
-                            {{--                            <div class="input-group">--}}
-                            {{--                                <div class="custom-file">--}}
-                            {{--                                    <input type="file" class="custom-file-input" id="user_file" name="user_file" value="" required>--}}
-                            {{--                                    <label class="custom-file-label" for="user_file"></label>--}}
-                            {{--                                </div>--}}
-                            {{--                            </div>--}}
-                            {{--                        </div>--}}
+                        {{--                        <div class="form-group">--}}
+                        {{--                            <label>.xlsx file*</label>--}}
+                        {{--                            <div class="input-group">--}}
+                        {{--                                <div class="custom-file">--}}
+                        {{--                                    <input type="file" class="custom-file-input" id="user_file" name="user_file" value="" required>--}}
+                        {{--                                    <label class="custom-file-label" for="user_file"></label>--}}
+                        {{--                                </div>--}}
+                        {{--                            </div>--}}
+                        {{--                        </div>--}}
 
-                            <div class="form-group">
-                                <label for="user_file">.xlsx file*</label>
-                                <input type="file" id="user_file" class="form-control" name="user_file">
-                            </div>
+                        <div class="form-group">
+                            <label for="user_file">.xlsx file*</label>
+                            <input type="file" id="user_file" class="form-control" name="user_file">
+                        </div>
 
-                            <div class="modal-footer">
-                                <button type="submit" class="btn btn-primary" id="saveBtn">Save changes</button>
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                            </div>
+                        <div class="modal-footer">
+                            <button type="submit" class="btn btn-primary" id="saveBtn">Save changes</button>
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        </div>
 
-                        </form>
-                    </div>
+                    </form>
                 </div>
             </div>
-            <!-- /modal -->
+        </div>
+        <!-- /modal -->
 
-            @push('scripts')
-                <script type="text/javascript">
+        @push('scripts')
+            <script type="text/javascript">
 
-                    $(function () {
+                $(function () {
 
-                        // csrf token
-                        $.ajaxSetup({
-                            headers: {
-                                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    // csrf token
+                    $.ajaxSetup({
+                        headers: {
+                            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                        }
+                    });
+
+                    // dataTable list
+                    var table = $('#basic-datatable').DataTable({
+                        processing: true,
+                        serverSide: true,
+                        ajax: {
+                            url: "{{ route('admin.requirements.index') }}",
+                            type: 'GET',
+                        },
+                        columns: [
+                            // {data: 'DT_RowIndex', name: 'DT_RowIndex'},
+                            {data: 'action', name: 'action', orderable: false, searchable: false},
+                            {data: 'rule_section', name: 'rule_section'},
+                            {data: 'rule_group', name: 'rule_group'},
+                            {data: 'rule_reference', name: 'rule_reference'},
+                            {data: 'rule_title', name: 'rule_title'},
+                            {data: 'rule_manual_reference', name: 'rule_manual_reference'},
+                            {data: 'rule_chapter', name: 'rule_chapter'},
+                        ],
+                        scrollY: 600,
+                        scrollX: true,
+                        iDisplayLength: -1,
+                        paging: false,
+                        language: {
+                            paginate: {
+                                previous: "<i class='mdi mdi-chevron-left'>",
+                                next: "<i class='mdi mdi-chevron-right'>"
                             }
-                        });
+                        },
+                        drawCallback: function () {
+                            $('.dataTables_paginate > .pagination').addClass('pagination-rounded');
+                        }
 
-                        // dataTable list
-                        var table = $('#basic-datatable').DataTable({
-                            processing: true,
-                            serverSide: true,
-                            ajax: {
-                                url: "{{ route('admin.requirements.index') }}",
-                                type: 'GET',
-                            },
-                            columns: [
-                                // {data: 'DT_RowIndex', name: 'DT_RowIndex'},
-                                {data: 'action', name: 'action', orderable: false, searchable: false},
-                                {data: 'rule_section', name: 'rule_section'},
-                                {data: 'rule_group', name: 'rule_group'},
-                                {data: 'rule_reference', name: 'rule_reference'},
-                                {data: 'rule_title', name: 'rule_title'},
-                                {data: 'rule_manual_reference', name: 'rule_manual_reference'},
-                                {data: 'rule_chapter', name: 'rule_chapter'},
-                            ],
-                            scrollY: 600,
-                            scrollX: true,
-                            iDisplayLength: -1,
-                            paging: false,
-                            language: {
-                                paginate: {
-                                    previous: "<i class='mdi mdi-chevron-left'>",
-                                    next: "<i class='mdi mdi-chevron-right'>"
+                    });
+
+                    // console log dataTable row
+                    $('#basic-datatable tbody').on('click', 'tr', function () {
+                        console.log(table.row(this).data());
+                    });
+
+                    // modal create
+                    $('#createNewItem').click(function () {
+                        var action = "{{ route('admin.requirements.store') }}";
+                        var method = "POST";
+
+                        resetForm();
+                        $('#ItemForm').trigger("reset");
+
+                        $('#modelHeading').html("Import Requirements");
+                        $('#ItemForm').attr('action', action); // form action
+                        $('#_method').val(method); // form method
+                        $('#saveBtn').html("Create"); // form button
+
+                        $('#title').val(''); //Add form data
+                        $('#description').val(''); //Add form data
+                        $('#user_file').val(''); //Add form data
+
+                        $('#ajaxModel').modal('show');
+                    });
+
+                    // reset form alert
+                    var resetForm = function () {
+                        $(".alert-success").remove();
+                        $(".text-danger").remove();
+                        $("form").find("input").removeClass('is-invalid');
+                    };
+
+
+                    // ajax import
+                    $('body').on('click', '#saveBtn', function (e) {
+                        e.preventDefault();
+                        resetForm();
+
+                        var form = $('#ItemForm');
+                        var formData = new FormData(form[0]);
+                        // var formData = new FormData(document.getElementById("ItemForm"));
+
+                        $.ajax({
+                            url: form.attr('action'),
+                            type: form.attr('method'),
+                            data: formData,
+                            cache: false,
+                            contentType: false,
+                            processData: false,
+                            success: function (data, textStatus, jqXHR) {
+                                console.log(data);
+                                if (data.success) {
+                                    form.before('<div class="alert alert-success" role="alert">' + data.message + '</div>');
+                                    table.draw();
+                                } else {
+                                    $.each(data.errors, function (input_name, input_error) {
+                                        form.find("input[name='" + input_name + "']").addClass('is-invalid').after('<span class="text-danger">' + input_error + '</span>');
+                                    });
                                 }
                             },
-                            drawCallback: function () {
-                                $('.dataTables_paginate > .pagination').addClass('pagination-rounded');
-                            }
-
                         });
+                    });
 
-                        // console log dataTable row
-                        $('#basic-datatable tbody').on('click', 'tr', function () {
-                            console.log(table.row(this).data());
-                        });
+                });// end function
 
-                        // modal create
-                        $('#createNewItem').click(function () {
-                            var action = "{{ route('admin.requirements.store') }}";
-                            var method = "POST";
-
-                            resetForm();
-                            $('#ItemForm').trigger("reset");
-
-                            $('#modelHeading').html("Import Requirements");
-                            $('#ItemForm').attr('action', action); // form action
-                            $('#_method').val(method); // form method
-                            $('#saveBtn').html("Create"); // form button
-
-                            $('#title').val(''); //Add form data
-                            $('#description').val(''); //Add form data
-                            $('#user_file').val(''); //Add form data
-
-                            $('#ajaxModel').modal('show');
-                        });
-
-                        // reset form alert
-                        var resetForm = function () {
-                            $(".alert-success").remove();
-                            $(".text-danger").remove();
-                            $("form").find("input").removeClass('is-invalid');
-                        };
-
-
-                        // ajax import
-                        $('body').on('click', '#saveBtn', function (e) {
-                            e.preventDefault();
-                            resetForm();
-
-                            var form = $('#ItemForm');
-                            var formData = new FormData(form[0]);
-                            // var formData = new FormData(document.getElementById("ItemForm"));
-
-                            $.ajax({
-                                url: form.attr('action'),
-                                type: form.attr('method'),
-                                data: formData,
-                                cache       : false,
-                                contentType : false,
-                                processData : false,
-                                success: function (data, textStatus, jqXHR) {
-                                    console.log(data);
-                                    if (data.success) {
-                                        form.before('<div class="alert alert-success" role="alert">' + data.message + '</div>');
-                                        table.draw();
-                                    } else {
-                                        $.each(data.errors, function (input_name, input_error) {
-                                            form.find("input[name='" + input_name + "']").addClass('is-invalid').after('<span class="text-danger">' + input_error + '</span>');
-                                        });
-                                    }
-                                },
-                            });
-                        });
-
-                    });// end function
-
-                </script>
+            </script>
     @endpush
 
 @endsection
